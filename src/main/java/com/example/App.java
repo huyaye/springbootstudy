e09534545039fcf62888ac8e58cca42ba7d53051
huyaye <huyaye@naver.com>
2018-01-13 21:29:03 +0900

    Connection the embedded memory DB Using Spring Data JPA

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,6 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.example.domain.Customer;
 import com.example.repository.CustomerRepository;
@@ -20,7 +23,16 @@ public class App implements CommandLineRunner {
 		Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
 		System.out.println(created + " is created!");
 
-		customerRepository.findAll().forEach(System.out::println);
+		customerRepository.findAllOrderByName().forEach(System.out::println);
+
+		// Paging
+		Pageable pageable = new PageRequest(1, 3);
+		Page<Customer> page = customerRepository.findAll(pageable);
+		System.out.println("한 페이지당 데이터 수=" + page.getSize());
+		System.out.println("현재 페이지=" + page.getNumber());
+		System.out.println("전체 페이지 수=" + page.getTotalPages());
+		System.out.println("전체 데이터 수=" + page.getTotalElements());
+		page.getContent().forEach(System.out::println);
 	}
 
 	public static void main(String[] args) {
