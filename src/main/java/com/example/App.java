9f3b66ad2a2e34a97884ec51a10d87fdc4c7bdc4
huyaye <huyaye@naver.com>
2018-01-13 23:16:17 +0900

    Apply Rest Web Service

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,40 +1,12 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-
-import com.example.domain.Customer;
-import com.example.repository.CustomerRepository;
 
 @EnableAutoConfiguration
 @ComponentScan
-public class App implements CommandLineRunner {
-	@Autowired
-	CustomerRepository customerRepository;
-
-	@Override
-	public void run(String... strings) throws Exception {
-		Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
-		System.out.println(created + " is created!");
-
-		customerRepository.findAllOrderByName().forEach(System.out::println);
-
-		// Paging
-		Pageable pageable = new PageRequest(1, 3);
-		Page<Customer> page = customerRepository.findAll(pageable);
-		System.out.println("한 페이지당 데이터 수=" + page.getSize());
-		System.out.println("현재 페이지=" + page.getNumber());
-		System.out.println("전체 페이지 수=" + page.getTotalPages());
-		System.out.println("전체 데이터 수=" + page.getTotalElements());
-		page.getContent().forEach(System.out::println);
-	}
-
+public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
--- src/main/java/com/example/service/CustomerService.java
+++ src/main/java/com/example/service/CustomerService.java
@@ -12,12 +12,24 @@ import com.example.repository.CustomerRepository;
 public class CustomerService {
 	@Autowired
 	CustomerRepository customerRepository;
-	
-	public Customer save(Customer customer) {
+
+	public List<Customer> findAll() {
+		return customerRepository.findAllOrderByName();
+	}
+
+	public Customer findOne(Integer id) {
+		return customerRepository.findOne(id);
+	}
+
+	public Customer create(Customer customer) {
 		return customerRepository.save(customer);
 	}
-	
-	public List<Customer> findAll() {
-		return customerRepository.findAll();
+
+	public Customer update(Customer customer) {
+		return customerRepository.save(customer);
+	}
+
+	public void delete(Integer id) {
+		customerRepository.delete(id);
 	}
 }
